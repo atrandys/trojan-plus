e1854dbffddd5f3036111e92fc799590fef084e2
atrandys <37266675+atrandys@users.noreply.github.com>
2020-12-14 22:31:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -138,7 +138,6 @@ EOF
         "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
         "sni": "",
         "alpn": [
-            "h2",
             "http/1.1"
         ],
         "reuse_session": true,
